f5115137b721ad4024c0fc6718edb7bda5901653
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-10-25 02:02:49 +0530

    first commit

---
+++ vibhuti.session.sql
@@ -0,0 +1,32 @@
+/*
+Data TYPES
+
+INT
+DECIMAL(M,N)
+M total digits, N total digits after decimal
+
+VARCHAR(l)
+store strings
+
+BLOB
+to store binary DATA
+
+DATE
+TIMESTAMP
+
+*/
+
+/*
+
+creating TABLE
+CREATE TABLE student (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(20),
+    major VARCHAR(20)
+);
+
+*/
+DESCRIBE student;
+
+
+
