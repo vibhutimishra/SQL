399f2457a1bbfdacc6a0d895e2de3cf6fd34e2c6
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-10-25 06:35:22 +0530

    queries added

--- vibhuti.session.sql
+++ vibhuti.session.sql
@@ -18,8 +18,8 @@ TIMESTAMP
 -- creating TABLE
 CREATE TABLE student_demo (
     student_id INT PRIMARY KEY,
-    student_name VARCHAR(20),
-    major VARCHAR(20)
+    student_name VARCHAR(20) NOT NULL,
+    major VARCHAR(20) DEFAULT "undecided"
 );
 
 -- describes student_demo schema
@@ -32,18 +32,22 @@ Select * From student_demo;
 INSERT INTO student_demo VALUES(1,"Jack","Biology");
 INSERT INTO student_demo VALUES(2,"Kate","Sociology");
 INSERT INTO student_demo(student_id,student_name) VALUES(3,"Claire");
-INSERT INTO student_demo VALUES(4,"Jack","Biology");
+INSERT INTO student_demo VALUES(4,"Mike","CS");
+
+-- delete a table
+DROP Table student_demo;
 
 
+UPDATE student_demo
+SET major = "Computer Science"
+where major = "CS";
 
+--delete a row
+INSERT INTO student_demo VALUES(5,"Jack","Biology");
+DELETE from student_demo
+where student_id=5;
 
-
-
-
-
-
-
-
+--Queries
 
 
 
